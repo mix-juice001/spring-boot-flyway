be904ddad46e8beb26c8c057cddf693f474ad0a7
takumasetoguchi <setoguchi.takuma@gmail.com>
2015-10-05 15:58:41 +0900

    inital commit.

---
+++ src/main/java/demo/WebConfiguration.java
@@ -0,0 +1,18 @@
+package demo;
+
+import org.h2.server.web.WebServlet;
+import org.springframework.boot.context.embedded.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+//1.3.0から不要になる
+//jdbc:h2:mem:testdb
+@Configuration
+public class WebConfiguration {
+    @Bean
+    ServletRegistrationBean h2servletRegistration(){
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean( new WebServlet());
+        registrationBean.addUrlMappings("/console/*");
+        return registrationBean;
+    }
+}
